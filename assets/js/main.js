de5efcaf256926b203f42936f7b4c48abcbd47a5
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-13 18:14:58 +0100

    avanzamento esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,32 +9,15 @@ $(document).ready(function() {
   $('.drop-menu').mouseenter(function() {
     $(this).children('div').show();
   });
-  // $('.plans').mouseenter(function() {
-  //   $(this).next().show();
-  // });
 
   //all'uscita del mouse da card hide
   $('.drop-menu-features').mouseleave(function() {
     $('.features-card').hide();
   });
 
-  // $('.plans-card').mouseleave(function() {
-  //   $(this).hide();
-  // });
-
   $('.drop-menu').mouseleave(function() {
     $(this).children('div').hide();
   });
-
-
-  // $('.plans').mouseleave(function() {
-  //   if ($('.plans-card').mouseenter() || $('.plans').mouseenter()) {
-  //     $('.plans-card').show();
-  //   } else if($('.plans').mouseleave() && $('.plans-card').mouseleave()){
-  //     $('.plans-card').hide();
-  //   }
-  //   // $(this).next().hide();
-  // });
 
 
 });
